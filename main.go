0685d7f82787a2bc8e4c0beb66e8cc2f2d7a5cce
siwonpada <psw041115@gmail.com>
2025-05-22 01:52:01 +0900

    setting swagger

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/School-meal-lover/backend/docs"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+// @title Grrrrr API
+// @version 1.0
+// @description The server for Grrrrr application.
+
+func main() {
+	router := gin.Default()
+
+	// Set up the API routes
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+	router.GET("/hello", Helloworld)
+
+	// Set up Swagger
+	docs.SwaggerInfo.Title = "Grrrrr API"
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	// Run the server
+	router.Run(":8080")
+}
+
+// @Summary Hello World
+// @Description Returns a hello world message
+// @Tags hello
+// @Accept  json
+// @Produce  json
+// @Success 200 {string} string "hello world"
+// @Router /hello [get]
+func Helloworld(g *gin.Context) {
+	g.JSON(http.StatusOK, "hello world")
+}
