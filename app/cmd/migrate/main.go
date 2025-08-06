5393cc2f44b462bce255390cc98d11c65dc2e832
Shiwon Park <psw041115@gmail.com>
2025-08-06 15:36:38 +0900

    reduce: env_num

--- app/cmd/migrate/main.go
+++ app/cmd/migrate/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -9,9 +10,15 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
-func main(){
-	databaseURL := os.Getenv("DATABASE_URL")
-  if databaseURL == "" {
+func main() {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	databaseURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, dbname)
+	if databaseURL == "" {
 		log.Fatal("DATABASE_URL environment variable is not set")
 	}
 
@@ -38,7 +45,7 @@ func main(){
 			log.Printf("Warning: Failed to close migration instance: source=%v, db=%v", sourceErr, dbErr)
 		}
 	}()
-	
+
 	version, dirty, err := m.Version()
 	if err != nil && err != migrate.ErrNoChange {
 		log.Printf("Warning: Could not get migration version: %v", err)
