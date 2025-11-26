e11df0f3387d47354a520395d9cc24c0287b199e
Shiwon Park <psw041115@gmail.com>
2025-11-26 20:07:18 +0900

    modify: env name

--- docs/docs.go
+++ docs/docs.go
@@ -483,7 +483,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             os.Getenv("BaseHost"),
+	Host:             os.Getenv("BASE_HOST"),
 	BasePath:         "/api/v1",
 	Schemes:          []string{"http", "https"},
 	Title:            "Grrrrr API",
